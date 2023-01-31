aa1bd57b83f536e555e5d51c891a8ee42792b782
obasooto segun <Obasootosegun@gmail.com>
2023-01-31 13:28:27 +0100

    all

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-zv2ppm1me!r05i$!kt5q25n4(k701p$yu6)$civb9t)7rwjynj"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
